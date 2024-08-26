110f4e60df1871c7add61365b39fe2a95e6e977c
BATENDER <vinicius.torres.san@gmail.com>
2024-08-26 20:09:29 -0300

    Think in a solution for challenge 7

--- C_basico/C-iniciante/desafios/dificeis/primos.c
+++ C_basico/C-iniciante/desafios/dificeis/primos.c
@@ -8,5 +8,19 @@ Dica: Um número primo é divisível apenas por 1 e por ele mesmo. Use um loop p
 */
 
 int main() {
+    int numPrimo, numIgualUm, numIgualNum, i;
+
+    printf("\n Digite um inteiro para descobrir se ele eh primo : ");
+    scanf("%d", &numPrimo);
+
+    if ((numPrimo == 0) || (numPrimo == 1)) {
+        printf("\nNao sao numeros primos!\n");
+    } else {
+        for (i = 1; i <= numPrimo; i++) {
+            if (((numPrimo / i) == numPrimo) && ((numPrimo / i) == 1))
+                
+        }
+    }
+
     return 0;
 }
--- C_basico/C-iniciante/desafios/medias/fatorial.c
+++ C_basico/C-iniciante/desafios/medias/fatorial.c
@@ -8,5 +8,17 @@ Dica: Use um loop for ou while para realizar a multiplicação sucessiva.
 */
 
 int main() {
+    int num, i;
+    int numFatorial = 1;
+
+    printf("\nDigite um inteiro para saber seu fatorial : ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; i++) {
+        numFatorial *= i;
+    }
+    
+    printf("\nO valor de %d! = %d", num, numFatorial);
+
     return 0;
 }
